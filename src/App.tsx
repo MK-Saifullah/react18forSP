557fb1e5846cdf47081e35c7bf15dd3884121d93
Khalid Saifullah <khalid@lyftaskrot.se>
2025-02-07 10:13:04 +0100

    2nd commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ const App: React.FC = () => {
     <div className="text-center">
       <h1 className="text-5xl font-bold underline text-center">Hello React </h1>
       <p className="font-bold">React 18.2.0</p>
+      <p>I need to inject it in sharepoint online page not using SPFx</p>
     </div>
   )
 }
