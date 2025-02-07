a9c4b4442ba056f2b28c8a0087e206c582679500
Khalid Saifullah <khalid@lyftaskrot.se>
2025-02-07 10:04:16 +0100

    commit react18 for sp

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App: React.FC = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-5xl font-bold underline text-center">Hello React </h1>
+      <p className="font-bold">React 18.2.0</p>
+    </div>
+  )
+}
+export default App
